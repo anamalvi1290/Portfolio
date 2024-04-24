639d2765154f84e6079ed088f867158cd1dedfa4
Anam Alvi <88087230+anamalvi1290@users.noreply.github.com>
2024-04-24 18:30:00 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,13 +18,13 @@ window.addEventListener('resize', toggleNavSmall);
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
-// const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav__link')
 
-// function linkAction(){
-// //     const navMenu = document.getElementById('nav-menu')
-// //     // When we click on each nav__link, we remove the show-menu class
-//     navMenu.classList.remove('show')
-// }
+function linkAction(){
+//     const navMenu = document.getElementById('nav-menu')
+//     // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
